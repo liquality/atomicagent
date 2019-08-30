8d0c98b42763f8f863a42164d70d05fc5c3559b9
Alex Griessel <alex.griessel@gmail.com>
2019-08-30 13:00:53 +0200

    Latest config changes for segwit

--- src/utils/clients.js
+++ src/utils/clients.js
@@ -1,18 +1,20 @@
 const Client = require('@liquality/client')
 
-const BitcoinBitcoreRpcProvider = require('@liquality/bitcoin-bitcore-rpc-provider')
-const BitcoinBitcoinJsLibSwapProvider = require('@liquality/bitcoin-bitcoinjs-lib-swap-provider')
+const BitcoinRpcProvider = require('@liquality/bitcoin-rpc-provider')
+const BitcoinNodeWalletProvider = require('@liquality/bitcoin-node-wallet-provider')
+const BitcoinSwapProvider = require('@liquality/bitcoin-swap-provider')
 const BitcoinNetworks = require('@liquality/bitcoin-networks')
 
 const EthereumRpcProvider = require('@liquality/ethereum-rpc-provider')
 const EthereumSwapProvider = require('@liquality/ethereum-swap-provider')
 
 const BTC = new Client()
-BTC.addProvider(new BitcoinBitcoreRpcProvider('http://localhost:4321/bitcoind/', 'bitcoin', 'local321'))
-BTC.addProvider(new BitcoinBitcoinJsLibSwapProvider({ network: BitcoinNetworks['bitcoin_testnet'] }))
+BTC.addProvider(new BitcoinRpcProvider('http://localhost:18443/', 'bitcoin', 'local321'))
+BTC.addProvider(new BitcoinNodeWalletProvider(BitcoinNetworks['bitcoin_regtest'], 'http://localhost:18443/', 'bitcoin', 'local321'))
+BTC.addProvider(new BitcoinSwapProvider({ network: BitcoinNetworks['bitcoin_regtest'] }))
 
 const ETH = new Client()
-ETH.addProvider(new EthereumRpcProvider('http://localhost:4321/parity/'))
+ETH.addProvider(new EthereumRpcProvider('http://localhost:8545/'))
 ETH.addProvider(new EthereumSwapProvider())
 
 module.exports = {
