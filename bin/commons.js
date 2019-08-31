cbfd0cd0ca422879f5595c0b49beee436246f015
Harsh Vakharia <harshjv@gmail.com>
2019-08-31 17:17:26 +0530

    add docker config

---
+++ bin/commons.js
@@ -0,0 +1,35 @@
+const program = require('commander')
+
+const CONFIG_ENV_MAP = {
+  port: 'PORT',
+  mongo: 'MONGODB_URI',
+  btcRpc: 'BTC_RPC',
+  btcUser: 'BTC_PASS',
+  btcPass: 'BTC_USER',
+  ethRpc: 'ETH_RPC',
+  ethUser: 'ETH_USER',
+  ethPass: 'ETH_PASS'
+}
+
+module.exports.loadVariables = (config = {}) => {
+  program
+    .option('-p, --port <port>', 'Application port', config.defaultPort ? config.defaultPort : 3000)
+    .option('--mongo <uri>', 'mongoDB uri', 'mongodb://localhost/agent')
+    .option('--btc-rpc <url>', 'Bitcoin RPC endpoint', 'http://localhost:18332')
+    .option('--btc-user <user>', 'Bitcoin RPC user', 'bitcoin')
+    .option('--btc-pass <pass>', 'Bitcoin RPC pass,', 'local321')
+    .option('--eth-rpc <url>', 'Ethereum RPC endpoint', 'http://localhost:8545')
+    .option('--eth-user <user>', 'Ethereum RPC user')
+    .option('--eth-pass <pass>', 'Ethereum RPC pass')
+
+  program
+    .parse(process.argv)
+
+  Object.entries(CONFIG_ENV_MAP).forEach(([configKey, envKey]) => {
+    if (!process.env[envKey]) {
+      process.env[envKey] = program[configKey]
+    }
+  })
+
+  process.env.PROCESS_TYPE = config.processType
+}
--- src/utils/clients.js
+++ src/utils/clients.js
@@ -1,5 +1,10 @@
 const Client = require('@liquality/client')
 
+const {
+  BTC_RPC, BTC_USER, BTC_PASS,
+  ETH_RPC, ETH_USER, ETH_PASS
+} = process.env
+
 const BitcoinRpcProvider = require('@liquality/bitcoin-rpc-provider')
 const BitcoinNodeWalletProvider = require('@liquality/bitcoin-node-wallet-provider')
 const BitcoinSwapProvider = require('@liquality/bitcoin-swap-provider')
@@ -9,12 +14,12 @@ const EthereumRpcProvider = require('@liquality/ethereum-rpc-provider')
 const EthereumSwapProvider = require('@liquality/ethereum-swap-provider')
 
 const BTC = new Client()
-BTC.addProvider(new BitcoinRpcProvider('http://localhost:18443/', 'bitcoin', 'local321'))
-BTC.addProvider(new BitcoinNodeWalletProvider(BitcoinNetworks['bitcoin_regtest'], 'http://localhost:18443/', 'bitcoin', 'local321'))
+BTC.addProvider(new BitcoinRpcProvider(BTC_RPC, BTC_USER, BTC_PASS))
+BTC.addProvider(new BitcoinNodeWalletProvider(BitcoinNetworks['bitcoin_regtest'], BTC_RPC, BTC_USER, BTC_PASS))
 BTC.addProvider(new BitcoinSwapProvider({ network: BitcoinNetworks['bitcoin_regtest'] }))
 
 const ETH = new Client()
-ETH.addProvider(new EthereumRpcProvider('http://localhost:8545/'))
+ETH.addProvider(new EthereumRpcProvider(ETH_RPC, ETH_USER, ETH_PASS))
 ETH.addProvider(new EthereumSwapProvider())
 
 module.exports = {
