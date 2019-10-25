9617db2e16622c50af1e86fb633d77a5a7543dc3
monokh <mnokhb@gmail.com>
2019-10-25 11:51:02 +0100

    Allow markets to be updated with migrate

--- src/migrate/index.js
+++ src/migrate/index.js
@@ -2,8 +2,9 @@ const Market = require('../models/Market')
 const markets = require('./data/markets.json')
 
 async function main () {
+  await Market.deleteMany({})
   const newMarkets = await Market.insertMany(markets, { ordered: false })
-  console.log(`${newMarkets.length} markets have been created`)
+  console.log(`${newMarkets.length} markets have been set`)
 }
 
 main()
