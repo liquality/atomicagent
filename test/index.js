8c1a5368aca8de0b14a7858c9066c93d63bffe7b
Alex Griessel <alex.griessel@gmail.com>
2019-10-31 08:21:27 +0200

    Patched up the simple tests

--- test/index.js
+++ test/index.js
@@ -5,13 +5,14 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-var server = "https://virtserver.swaggerhub.com/monokh/liquality-agent/1.0.0/swap";
+//var server = "https://virtserver.swaggerhub.com/monokh/liquality-agent/1.0.0/swap";
+var server = "http://localhost:3030/api/swap";
 
 
 describe('marketinfo', () => {
   beforeEach((done) => { //Before each test we empty the database
  //   console.log("Before")
-    done();           
+    done();
   });
 
   describe('/GET marketinfo', () => {
@@ -21,53 +22,52 @@ describe('marketinfo', () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('array');
-          res.body.length.should.be.eql(1);
+          res.body.length.should.be.eql(3);
           done();
         });
     });
   });
 
   describe('/POST order', () => {
+    var quote = {}
+
     it('it should POST to get a quote', (done) => {
       chai.request(server)
         .post('/order')
         .send({
           "from": "BTC",
           "to": "ETH",
-          "fromAmount": 51000000
+          "fromAmount": 5000000
         })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
+          quote = res;
           done();
         });
     });
-  });
 
-
-  describe('/POST order', () => {
-    it('it should POST to get a quote', (done) => {
+    it('it should GET information by quote id', (done) => {
       chai.request(server)
-        .post('/order')
+        .get('/order/' + quote.body.orderId)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.orderId.should.equal(quote.body.orderId);
+          done();
+        });
+    });
+
+    it('it should POST to confirm the quote', (done) => {
+      chai.request(server)
+        .post('/order/' + quote.body.orderId)
         .send({
-          "fromAddress": "mgxDATSdgJ3eYSj7ukYeN6QNr7gofUa3Tk",
-          "toAddress": "0x342800907eE13A0c7Bd9d6ebA13A417ba79c5b0b",
-          "fundHash": "39eaf9d29d118478187ce164a5baf9429dd805ef5e4c5f4387719207295a90d0",
-          "secretHash": "16d54b643a30110ec13b4122231a0981dae0c3f8e94642222d85d98179e1b5a8",
-          "swapExpiration": 1562386069
+          "fromAddress": "0x572E7610B0FC9a00cb4A441F398c9C7a5517DE32",
+          "toAddress": "bcrt1qjywshhj05s0lan3drpv9cu7t595y7k5x00ttf8",
+          "fromFundHash": "98241f985c22fa523028f5fbc7d61305f8ee11fce7c334f015a470f292624948",
+          "secretHash": "122f75aa0dbfb90db7984fe82400888443eacca84d388c7a93d976c640864e01",
+          "swapExpiration": 1566603605
         })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          done();
-        });
-    });
-  });
-
-  describe('/GET order by id eae4d76c-3ba2-465d-930b-324fcbe17d95', () => {
-    it('it should GET the status of a quote by id', (done) => {
-      chai.request(server)
-        .get('/order/eae4d76c-3ba2-465d-930b-324fcbe17d95')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
