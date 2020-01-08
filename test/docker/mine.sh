bfcae6ed71c7d14b095395accbf798256ab6f057
Harsh Vakharia <harshjv@gmail.com>
2020-01-08 21:30:01 +0530

    bump waiting time

--- test/docker/mine.sh
+++ test/docker/mine.sh
@@ -1,7 +1,7 @@
 mine_btc () { curl --basic -u bitcoin:local321 -X POST -d "{\"jsonrpc\": \"2.0\", \"id\": \"0\", \"method\": \"generate\", \"params\": [$1]}" http://${BTC_HOST}:${BTC_PORT}; }
 mine_eth () { curl -X POST -d '{"jsonrpc": "2.0", "method": "eth_sendTransaction", "params": [{"from":"0xec98c8F5724D286faEd52270538677D7959748D5","to":"0xec98c8F5724D286faEd52270538677D7959748D5","value":"0xfffff"}], "id": 1}' http://${ETH_HOST}:${ETH_PORT}; }
 
-sleep 10
+sleep 30
 mine_btc 100
 
 while true; do
