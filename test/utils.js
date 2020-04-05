51051db6a120fd4eb3ec164f66fa5b57ca40a41a
Harsh Vakharia <harshjv@gmail.com>
2020-04-05 17:38:38 +0530

    reduce sleep timer in tests for API requests

--- test/utils.js
+++ test/utils.js
@@ -198,7 +198,7 @@ module.exports.swap = (from, to, fromAmount, refund, lateClaim) => {
     it('should verify funding of the quote', async function () {
       this.timeout(30 * 1000)
 
-      const check = () => sleep(5000).then(() => chai.request(app())
+      const check = () => sleep(1000).then(() => chai.request(app())
         .get(`/api/swap/order/${quote.orderId}`)
         .then(res => {
           res.should.have.status(200)
@@ -216,7 +216,7 @@ module.exports.swap = (from, to, fromAmount, refund, lateClaim) => {
     it('should reciprocate by funding the swap', async function () {
       this.timeout(30 * 1000)
 
-      const check = () => sleep(5000).then(() => chai.request(app())
+      const check = () => sleep(1000).then(() => chai.request(app())
         .get(`/api/swap/order/${quote.orderId}`)
         .then(res => {
           res.should.have.status(200)
@@ -283,7 +283,7 @@ module.exports.swap = (from, to, fromAmount, refund, lateClaim) => {
 
       const expectedStatus = refund ? 'AGENT_REFUNDED' : 'AGENT_CLAIMED'
 
-      const check = () => sleep(5000).then(() => chai.request(app())
+      const check = () => sleep(1000).then(() => chai.request(app())
         .get(`/api/swap/order/${quote.orderId}`)
         .then(res => {
           res.should.have.status(200)
