b71db8e853ab6e4e884e49e1f89bca6c13af5739
Harsh Vakharia <harshjv@gmail.com>
2020-12-01 20:28:16 +0530

    add support for fromRefundHash and txMap

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,11 +3,15 @@ const { v4: uuidv4 } = require('uuid')
 const assets = require('@liquality/cryptoassets').default
 
 const config = require('../config')
+const AuditLog = require('./AuditLog')
 const MarketHistory = require('./MarketHistory')
 
 const { getClient } = require('../utils/clients')
+const { withLock } = require('../utils/chainLock')
 const crypto = require('../utils/crypto')
+const { RescheduleError } = require('../utils/errors')
 const { calculateToAmount, calculateUsdAmount } = require('../utils/fx')
+const blockScanOrFind = require('../utils/blockScanOrFind')
 
 const OrderSchema = new mongoose.Schema({
   orderId: {
@@ -85,6 +89,10 @@ const OrderSchema = new mongoose.Schema({
     type: String,
     index: true
   },
+  fromRefundHash: {
+    type: String,
+    index: true
+  },
   fromClaimHash: {
     type: String,
     index: true
@@ -122,14 +130,27 @@ const OrderSchema = new mongoose.Schema({
     index: true
   },
 
-  fees: {
+  txMap: {
     type: Object,
-    default: {
-      // [txHash]: {
-      //   amount: 1111,
-      //   usd: 12
-      // }
-    }
+    default: {}
+  },
+
+  hasAgentUnconfirmedTx: {
+    type: Boolean,
+    default: true,
+    index: true
+  },
+
+  hasUserUnconfirmedTx: {
+    type: Boolean,
+    default: true,
+    index: true
+  },
+
+  hasUnconfirmedTx: {
+    type: Boolean,
+    default: true,
+    index: true
   },
 
   passphraseHash: {
@@ -146,18 +167,15 @@ const OrderSchema = new mongoose.Schema({
     type: Number
   },
 
-  // pending: ['AGENT_FUNDED_UNVERIFIED', 'AGENT_FUNDED', 'USER_CLAIMED', 'AGENT_CLAIMED_UNVERIFIED', 'AGENT_CLAIMED', 'AGENT_CLAIMED_UNVERIFIED', 'AGENT_CLAIMED']
-  // success: ['AGENT_CLAIMED', 'AGENT_REFUNDED']
-
   status: {
     type: String,
     enum: [
       'QUOTE',
       'USER_FUNDED_UNVERIFIED', 'USER_FUNDED',
-      'AGENT_FUNDED_UNVERIFIED', 'AGENT_FUNDED',
+      'AGENT_FUNDED',
       'USER_CLAIMED',
-      'AGENT_CLAIMED_UNVERIFIED', 'AGENT_CLAIMED',
-      'AGENT_REFUNDED_UNVERIFIED', 'AGENT_REFUNDED',
+      'AGENT_CLAIMED',
+      'AGENT_REFUNDED',
       'QUOTE_EXPIRED',
       'SWAP_EXPIRED'
     ],
@@ -229,6 +247,246 @@ OrderSchema.methods.setUsdRates = async function () {
 
   this.fromAmountUsd = calculateUsdAmount(this.from, this.fromAmount, fromRateUsd)
   this.toAmountUsd = calculateUsdAmount(this.to, this.toAmount, toRateUsd)
+}
+
+OrderSchema.pre('save', function (next) {
+  const txs = Object.values(this.txMap)
+
+  const userTxs = txs.filter(
+    ({ type }) => (type.startsWith('from') && !type.includes('Claim')) || (type.startsWith('to') && type.includes('Claim'))
+  )
+
+  const agentTxs = txs.filter(
+    ({ type }) => (type.startsWith('to') && !type.includes('Claim')) || (type.startsWith('from') && type.includes('Claim'))
+  )
+
+  this.hasUserUnconfirmedTx = !userTxs.every(({ blockHash }) => blockHash)
+  this.hasAgentUnconfirmedTx = !agentTxs.every(({ blockHash }) => blockHash)
+  this.hasUnconfirmedTx = this.hasUserUnconfirmedTx || this.hasAgentUnconfirmedTx
+
+  next()
+})
+
+OrderSchema.methods.isQuoteExpired = function () {
+  return Date.now() > this.expiresAt
+}
+
+OrderSchema.methods.isSwapExpired = function (fromCurrentBlock) {
+  return fromCurrentBlock.timestamp >= this.swapExpiration
+}
+
+OrderSchema.methods.isNodeSwapExpired = function (toCurrentBlock) {
+  return toCurrentBlock.timestamp >= this.nodeSwapExpiration
+}
+
+OrderSchema.methods.addTx = function (type, tx) {
+  if (!type) throw new Error('Invalid type')
+  if (!tx || !tx.hash) throw new Error('Invalid tx')
+
+  let side = type.match(/^from|^to/)
+  if (!side) throw new Error(`Invalid tx type: ${type}`)
+  side = side[0]
+
+  const asset = this[side]
+  const key = `txMap.${tx.hash}`
+  const value = { asset, type, hash: tx.hash }
+
+  if (tx.fee || tx.feePrice) {
+    value.feeAmount = tx.fee
+    value.feePrice = tx.feePrice
+    value.feeAmountUsd = calculateUsdAmount(asset, tx.fee, this[`${side}RateUsd`]).toNumber()
+  }
+
+  if (tx.blockHash) {
+    value.blockHash = tx.blockHash
+    value.blockNumber = tx.blockNumber
+  }
+
+  if (tx.placeholder) {
+    value.placeholder = true
+  } else {
+    this.set(type, tx.hash)
+  }
+
+  Object.values(this.txMap).forEach(t => {
+    if (t.type === type && t.placeholder) {
+      this.set(`txMap.${t.hash}`, undefined)
+    }
+  })
+
+  this.set(key, value)
+}
+
+OrderSchema.methods.claimSwap = async function () {
+  const fromClient = this.fromClient()
+  const { defaultFee } = config.assets[this.from]
+
+  return withLock(this.from, async () => {
+    const fees = await fromClient.chain.getFees()
+
+    return fromClient.swap.claimSwap(
+      this.fromFundHash,
+      this.fromCounterPartyAddress,
+      this.fromAddress,
+      this.secret,
+      this.swapExpiration,
+      fees[defaultFee].fee
+    )
+  })
+}
+
+OrderSchema.methods.refundSwap = async function () {
+  const toClient = this.toClient()
+  const { defaultFee } = config.assets[this.to]
+
+  return withLock(this.from, async () => {
+    const fees = await toClient.chain.getFees()
+
+    const refundTx = await toClient.swap.refundSwap(
+      this.toFundHash,
+      this.toAddress,
+      this.toCounterPartyAddress,
+      this.secretHash,
+      this.nodeSwapExpiration,
+      fees[defaultFee].fee
+    )
+
+    return refundTx
+  })
+}
+
+OrderSchema.methods.initiateSwap = async function () {
+  const toClient = this.toClient()
+  const { defaultFee } = config.assets[this.to]
+
+  return withLock(this.to, async () => {
+    const fees = await toClient.chain.getFees()
+
+    return toClient.swap.initiateSwap(
+      this.toAmount,
+      this.toAddress,
+      this.toCounterPartyAddress,
+      this.secretHash,
+      this.nodeSwapExpiration,
+      fees[defaultFee].fee
+    )
+  })
+}
+
+OrderSchema.methods.verifyInitiateSwapTransaction = async function () {
+  const fromClient = this.fromClient()
+
+  try {
+    const verified = await fromClient.swap.verifyInitiateSwapTransaction(
+      this.fromFundHash,
+      this.fromAmount,
+      this.fromCounterPartyAddress,
+      this.fromAddress,
+      this.secretHash,
+      this.swapExpiration
+    )
+
+    if (!verified) {
+      throw new RescheduleError(`Reschedule ${this.orderId}: Transaction not found`, this.from)
+    }
+  } catch (e) {
+    if (['TxNotFoundError', 'PendingTxError', 'RescheduleError'].includes(e.name)) {
+      throw new RescheduleError(e.message, this.from)
+    }
+
+    throw e
+  }
+}
+
+OrderSchema.methods.findFundSwapTransaction = async function () {
+  const fromClient = this.fromClient()
+
+  try {
+    const fromSecondaryFundTx = await fromClient.swap.findFundSwapTransaction(
+      this.fromFundHash,
+      this.fromAmount,
+      this.fromCounterPartyAddress,
+      this.fromAddress,
+      this.secretHash,
+      this.swapExpiration
+    )
+
+    return fromSecondaryFundTx
+  } catch (e) {
+    if (['TxNotFoundError', 'PendingTxError'].includes(e.name)) {
+      throw new RescheduleError(e.message, this.from)
+    }
+
+    throw e
+  }
+}
+
+OrderSchema.methods.findRefundSwapTransaction = async function (fromLastScannedBlock, fromCurrentBlockNumber) {
+  const fromClient = this.fromClient()
+
+  if (!fromCurrentBlockNumber) {
+    fromCurrentBlockNumber = await fromClient.chain.getBlockHeight()
+  }
+
+  return blockScanOrFind(fromClient, async blockNumber => {
+    try {
+      const tx = await fromClient.swap.findRefundSwapTransaction(
+        this.fromFundHash,
+        this.fromCounterPartyAddress,
+        this.fromAddress,
+        this.secretHash,
+        this.swapExpiration,
+        blockNumber
+      )
+
+      return tx
+    } catch (e) {
+      if (['PendingTxError', 'BlockNotFoundError'].includes(e.name)) {
+        throw new RescheduleError(e.message, this.from)
+      }
+
+      throw e
+    }
+  }, fromLastScannedBlock, fromCurrentBlockNumber)
+}
+
+OrderSchema.methods.findClaimSwapTransaction = async function (toLastScannedBlock, toCurrentBlockNumber) {
+  const toClient = this.toClient()
+
+  if (!toCurrentBlockNumber) {
+    toCurrentBlockNumber = await toClient.chain.getBlockHeight()
+  }
+
+  return blockScanOrFind(toClient, async blockNumber => {
+    try {
+      const tx = await toClient.swap.findClaimSwapTransaction(
+        this.toFundHash,
+        this.toAddress,
+        this.toCounterPartyAddress,
+        this.secretHash,
+        this.nodeSwapExpiration,
+        blockNumber
+      )
+
+      return tx
+    } catch (e) {
+      if (['PendingTxError', 'BlockNotFoundError'].includes(e.name)) {
+        throw new RescheduleError(e.message, this.to)
+      }
+
+      throw e
+    }
+  }, toLastScannedBlock, toCurrentBlockNumber)
+}
+
+OrderSchema.methods.log = async function (context, status, extra) {
+  return AuditLog.create({
+    orderId: this.orderId,
+    orderStatus: this.status,
+    context,
+    status,
+    extra
+  })
 }
 
 OrderSchema.static('fromMarket', function (market, fromAmount) {
