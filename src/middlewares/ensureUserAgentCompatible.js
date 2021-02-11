bc83ba1960beac9086fcd1c913621cbbd420fc5d
Harsh Vakharia <harshjv@gmail.com>
2021-02-11 22:28:00 +0530

    bump min CAL compatible version to 0.8.15

--- src/middlewares/ensureUserAgentCompatible.js
+++ src/middlewares/ensureUserAgentCompatible.js
@@ -1,6 +1,6 @@
 const semver = require('semver')
 
-const MIN_COMPATIBLE_CAL_VERSION = '0.8.11'
+const MIN_COMPATIBLE_CAL_VERSION = '0.8.15'
 const USER_AGENT_REGEX = /Wallet (\d.*?\.\d.*?\.\d.*?) \(CAL (\d.*?\.\d.*?\.\d.*?)\)/
 
 module.exports = (incompatibleResponse) => (req, res, next) => {
