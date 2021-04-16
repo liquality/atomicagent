c937f44534bfea303f86cc13653fe0997e247710
Harsh Vakharia <harshjv@gmail.com>
2021-04-16 13:34:44 +0530

    add retry/ignore mechanism

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -18,6 +18,25 @@ const ALLOWED_ACTIONS = [
   'reject'
 ]
 
+const ALLOWED_RETRY_JOBS = [
+  {
+    name: 'reciprocate-init-swap',
+    setStatus: 'USER_FUNDED'
+  },
+  {
+    name: 'fund-swap',
+    setStatus: 'AGENT_CONTRACT_CREATED'
+  },
+  {
+    name: 'find-claim-tx-or-refund',
+    setStatus: 'AGENT_FUNDED'
+  },
+  {
+    name: 'agent-claim',
+    setStatus: 'USER_CLAIMED'
+  }
+]
+
 router.post('/login', asyncHandler(async (req, res) => {
   const { body } = req
 
@@ -69,6 +88,75 @@ router.get('/order', ensureAuth(401), asyncHandler(async (req, res) => {
 
   const check = await Check.findOne({ orderId: orderId }).exec()
   res.ok(check)
+}))
+
+router.post('/order/retry', ensureAuth(401), asyncHandler(async (req, res) => {
+  const agenda = req.app.get('agenda')
+  const { body } = req
+  const { orderId, jobName } = body
+
+  if (!orderId) {
+    return res.notOk(400, 'Order ID missing')
+  }
+
+  if (!ALLOWED_RETRY_JOBS.includes(jobName)) {
+    return res.notOk(400, `Invalid job name: ${jobName}`)
+  }
+
+  const order = await Order.findOne({ orderId: orderId }).exec()
+  if (!order) {
+    return res.notOk(400, `Order not found: ${orderId}`)
+  }
+
+  const index = ALLOWED_RETRY_JOBS.findIndex(job => job.name === jobName)
+  const jobsToBeRemoved = ALLOWED_RETRY_JOBS.slice(index).map(job => job.name)
+
+  await agenda.cancel({
+    name: {
+      $in: jobsToBeRemoved
+    },
+    'data.orderId': orderId
+  })
+
+  order.status = ALLOWED_RETRY_JOBS[index].setStatus
+  await order.save()
+
+  await agenda.now(jobName, { orderId: order.orderId })
+
+  await order.log('RETRY', jobName)
+
+  res.ok()
+}))
+
+router.post('/order/ignore', ensureAuth(401), asyncHandler(async (req, res) => {
+  const { body } = req
+  const { orderId, jobName } = body
+
+  if (!orderId) {
+    return res.notOk(400, 'Order ID missing')
+  }
+
+  if (!ALLOWED_RETRY_JOBS.includes(jobName)) {
+    return res.notOk(400, `Invalid job name: ${jobName}`)
+  }
+
+  const order = await Order.findOne({ orderId: orderId }).exec()
+  if (!order) {
+    return res.notOk(400, `Order not found: ${orderId}`)
+  }
+
+  Object
+    .entries(order.txMap)
+    .forEach(([key, value]) => {
+      if (!value.blockHash) {
+        order.set(`txMap.${key}.replacedBy`, true)
+      }
+    })
+
+  order.status = 'QUOTE_EXPIRED'
+  await order.save()
+
+  res.ok()
 }))
 
 router.post('/order', ensureAuth(401), asyncHandler(async (req, res) => {
