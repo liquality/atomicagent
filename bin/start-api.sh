c02a3cc83c6eda3518b1cdab664f9a4b72f19790
|M| <manicprone@gmail.com>
2021-08-12 12:05:07 -0600

    Adding db test

--- bin/start-api.sh
+++ bin/start-api.sh
@@ -5,7 +5,11 @@ echo "ENV_ALIAS: ${ENV_ALIAS}"
 echo "--------------------------------"
 echo ""
 
+# echo "Running migrator..."
+
+# npm run migrate
+
 echo "Starting Atomic Agent API..."
 
 # npm run api
-npm run apit
+npm run api-test
--- src/test.js
+++ src/test.js
@@ -4,16 +4,20 @@ const config = require('./config')
 console.log('[DEVING] process.env.CONFIG_PATH:', process.env.CONFIG_PATH)
 // console.log('[DEVING] config:', config)
 
+// Load options
 const mongoUri = config.database.uri
 const mongoDebug = config.database.debug
 
 // Build connection to Mongo
 if (mongoDebug) mongoose.set('debug', true)
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
 mongoose.connection
   .on('error', console.error.bind(console, 'connection error:'))
   .once('open', () => console.info('Connected to MongoDB.'))
-
 
 // Create schema/model for testing
 const CharacterSchema = new mongoose.Schema({
