6d1d8faf29d4a4b48464259f7349b9958ea0e3f6
|M| <manicprone@gmail.com>
2021-08-13 08:48:04 -0600

    Supporting log, verbose options for migrate

--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
+const mongoose = require('mongoose')
 const Sentry = require('@sentry/node')
+const config = require('./config')
 
+// Enable Sentry (for production only)
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: process.env.SENTRY_DSN
   })
 }
 
-const mongoose = require('mongoose')
-const config = require('./config')
-
-const forceMigrate = config.database.forceMigrate
-
+// Load DB settings
+const forceMigrate = config.database.migrate.force
+const logMigrate = config.database.migrate.log
+const verboseMigrate = config.database.migrate.verbose
 if (config.database.debug) {
   mongoose.set('debug', true)
 }
 
+// Connect to DB
 const mongooseOnError = err => {
   console.error(err)
   process.exit(1)
 }
 
 mongoose
-  .connect(config.database.uri, { useNewUrlParser: true, useCreateIndex: true })
+  .connect(config.database.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
   .catch(mongooseOnError)
 
 mongoose
   .connection.on('error', mongooseOnError)
 
+// Run service
 switch (process.env.PROCESS_TYPE) {
   case 'api':
     require('./api').start()
@@ -37,7 +45,11 @@ switch (process.env.PROCESS_TYPE) {
     break
 
   case 'migrate':
-    require('./migrate').run({ force: forceMigrate })
+    require('./migrate').run({
+      force: forceMigrate,
+      log: logMigrate,
+      verbose: verboseMigrate
+    })
     break
 
   default:
--- src/migrate/index.js
+++ src/migrate/index.js
@@ -5,41 +5,43 @@ const assets = require('./data/assets.json')
 const markets = require('./data/markets.json')
 
 const defaultLog = true
+const defaultVerbose = false
 const defaultForce = false
 const logHeader = '[MIGRATE]'
 
 module.exports.run = async (options = {}) => {
   console.log(`${logHeader} Running migrate with options:`, options)
   const log = objectUtils.get(options, 'log', defaultLog)
+  const verbose = objectUtils.get(options, 'verbose', defaultVerbose)
   const force = objectUtils.get(options, 'force', defaultForce)
 
-  if (await hasData() && !force) {
-    console.log(`${logHeader} Data is already seeded.`)
+  if (!force && await hasData({ verbose })) {
+    if (log) console.log(`${logHeader} Data is already seeded.`)
   } else {
-    console.log(`${logHeader} Seeding data...`)
+    if (log) console.log(`${logHeader} Seeding data...`)
     await Asset.deleteMany({})
     const newAssets = await Asset.insertMany(assets, { ordered: false })
-    console.log(`${logHeader} ${newAssets.length} assets have been set`)
+    if (log) console.log(`${logHeader} ${newAssets.length} assets have been set`)
 
     await Market.deleteMany({})
     const newMarkets = await Market.insertMany(markets, { ordered: false })
-    console.log(`${logHeader} ${newMarkets.length} markets have been set`)
+    if (log) console.log(`${logHeader} ${newMarkets.length} markets have been set`)
   }
 }
 
-async function hasData () {
+async function hasData (options = {}) {
+  const verbose = options.verbose
   let result = true
 
-  console.log(`${logHeader} Checking for existing Assets`)
-  // console.log(`${logHeader}:`, db.runCommand( { listCollections: 1 } ))
+  if (verbose) console.log(`${logHeader} Checking for existing assets`)
 
   const existingAssets = await Asset.find({}).exec()
 
   if (!existingAssets || existingAssets.length === 0) {
-    console.log('No assets found.')
+    if (verbose) console.log('No assets found.')
     result = false
   } else {
-    console.log('Existing assets:', existingAssets)
+    if (verbose) console.log('Existing assets:', existingAssets)
   }
 
   return result
