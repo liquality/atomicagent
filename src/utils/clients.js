fb6c284442ae70d1cd993e047688377a4c4054ea
Harsh Vakharia <2311316+harshjv@users.noreply.github.com>
2022-02-22 12:46:48 +0530

    add support for AVAX

--- src/utils/clients.js
+++ src/utils/clients.js
@@ -250,6 +250,7 @@ async function createClient(asset) {
   if (assetData.chain === 'rsk') return createEthClient(asset)
   if (assetData.chain === 'bsc') return createEthClient(asset)
   if (assetData.chain === 'polygon') return createEthClient(asset)
+  if (assetData.chain === 'avalanche') return createEthClient(asset)
   if (assetData.chain === 'arbitrum') return createEthClient(asset)
   if (assetData.chain === 'ethereum') return createEthClient(asset)
   if (assetData.chain === 'near') return createNearClient()
