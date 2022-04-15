145347fb719fe1f258e8ec3f3db6cc91ec77e304
devaraj <devarajsuk@gmail.com>
2022-04-15 10:46:24 +0100

    chore(interval): update every one min

---
+++ src/syncMarketData.js
@@ -0,0 +1,42 @@
+const Sentry = require('@sentry/node')
+
+// Enable Sentry (for production only)
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN
+  })
+}
+
+const worker = require('./syncMarketData')
+const mongo = require('./utils/mongo')
+
+mongo.connect()
+
+async function start() {
+  await worker.start()
+}
+
+function stop(signal) {
+  return async function () {
+    console.log('Received', signal)
+
+    await worker.stop()
+    await mongo.disconnect()
+    process.exit(0)
+  }
+}
+
+switch (process.env.PROCESS_TYPE) {
+  case 'migrate':
+    require('./migrate').run()
+    break
+
+  default: {
+    start()
+
+    process.on('SIGTERM', stop('SIGTERM'))
+    process.on('SIGINT', stop('SIGINT'))
+
+    break
+  }
+}
